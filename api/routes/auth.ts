eff18fc36681570e23e8c314a843c21f89ae7ca4
Barend Erasmus <developersworkspace@gmail.com>
2017-01-21 11:46:11 +0200

    Update

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -2,19 +2,13 @@
 import { Express, Request, Response } from "express";
 import { config } from './../config';
 import { AuthService } from './../../core/services/auth';
+import * as base64 from 'base-64';
+import * as utf8 from 'utf8';
 import * as clientOAuth2 from 'client-oauth2';
 
 let express = require('express');
 let router = express.Router();
 
-let githubAuth = new clientOAuth2({
-    clientId: '2e5099132d37735f7e1e',
-    clientSecret: '29d9ab22b8445f04808bd142dc1550adc0e0082a',
-    accessTokenUri: 'https://github.com/login/oauth/access_token',
-    authorizationUri: 'https://github.com/login/oauth/authorize',
-    redirectUri: 'http://localhost:9009/api/auth/github/callback',
-    scopes: 'user:email'
-}, null)
 
 router.get('/authorize', function (req: Request, res: Response, next: Function) {
     let authService = new AuthService();
@@ -61,12 +55,20 @@ router.get('/token', function (req: Request, res: Response, next: Function) {
 
 
 router.get('/github', function (req, res) {
-    var uri = githubAuth.code.getUri();
+    let auth = createAuth(req.query.uri);
+    var uri = auth.githubAuth.code.getUri();
+    res.redirect(uri);
+});
+
+router.get('/google', function (req, res) {
+    let auth = createAuth(req.query.uri);
+    var uri = auth.googleAuth.code.getUri();
     res.redirect(uri);
 });
 
 router.get('/github/callback', function (req, res) {
-    githubAuth.code.getToken(req.originalUrl)
+    let auth = createAuth('');
+    auth.githubAuth.code.getToken(req.originalUrl)
         .then((user: any) => {
             console.log(user);
 
@@ -75,5 +77,49 @@ router.get('/github/callback', function (req, res) {
             return res.send('a');
         });
 });
+
+router.get('/google/callback', function (req, res) {
+    let auth = createAuth('');
+    auth.googleAuth.code.getToken(req.originalUrl)
+        .then((user: any) => {
+            console.log(user);
+
+            return res.send(user.accessToken);
+        }).catch((err: Error) => {
+            return res.send('a');
+        });
+});
+
+
+function createAuth(redirectUri: string) {
+
+    var bytes = utf8.encode(redirectUri);
+    var encodedRedirectUri = base64.encode(bytes);
+
+    let githubAuth = new clientOAuth2({
+        clientId: '2e5099132d37735f7e1e',
+        clientSecret: '29d9ab22b8445f04808bd142dc1550adc0e0082a',
+        accessTokenUri: 'https://github.com/login/oauth/access_token',
+        authorizationUri: 'https://github.com/login/oauth/authorize',
+        redirectUri: 'http://localhost:9009/api/auth/github/callback',
+        scopes: 'user:email',
+        state: encodedRedirectUri
+    }, null);
+
+    let googleAuth = new clientOAuth2({
+        clientId: '136693745519-hl3n0m72r4hpc1vpc49kgg804120vv5t.apps.googleusercontent.com',
+        clientSecret: 'ily5DezHxdqm4xXrsqBIZ9pn',
+        accessTokenUri: 'https://www.googleapis.com/oauth2/v4/token',
+        authorizationUri: 'https://accounts.google.com/o/oauth2/v2/auth',
+        redirectUri: 'http://localhost:9009/api/auth/google/callback',
+        scopes: 'https://www.googleapis.com/auth/userinfo.email',
+        state: encodedRedirectUri
+    }, null);
+
+    return {
+        githubAuth: googleAuth,
+        googleAuth: googleAuth
+    };
+}
 
 export = router;
